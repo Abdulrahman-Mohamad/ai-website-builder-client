c06888bca63fa96cd7a455e6bafff1e7ddcc63b2
Abdulrahman-Mohamad <abd.rahman.mohamad.93@gmail.com>
2025-12-22 07:03:06 +0200

    fix warning

--- src/pages/View.tsx
+++ src/pages/View.tsx
@@ -24,7 +24,6 @@ const View = () => {
   }
 
   useEffect(() => {
-    // eslint-disable-next-line react-hooks/set-state-in-effect
     fetchCode()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
